82765faae747d9cf400e7f942941da5e615348cf
izzat <izzatfadzlon@gmail.com>
2025-02-12 23:36:04 +0800

    Initialize Home, Statement, and Transfer pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { HashRouter, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Statement from "./pages/Statement";
+import Transfer from "./pages/Transfer";
 
 function App() {
   return (
     <HashRouter>
       <Navbar />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/statement" element={<Statement />} />
+        <Route path="/transfer" element={<Transfer />} />
+      </Routes>
     </HashRouter>
   );
 }
