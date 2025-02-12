165eec11c0baea3d924efc133d8cb1f9ec1963a1
izzat <izzatfadzlon@gmail.com>
2025-02-12 21:27:31 +0800

    Enhance navigation bar with additional menu items and improve styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ function App() {
         <div className="flex flex-row justify-center items-center h-full">
           <div className="w-4/5 flex flex-row justify-between items-center">
             <div className="flex flex-row items-center">
-              <img src="gic3.png" className="w-16"></img>
-              <p className="font-semibold text-gray-300 text-xl text-center mx-5">
+              <img src="gic3.png" className="min-w-16 w-16 "></img>
+              <p className="font-semibold text-white opacity-70 hover:opacity-100 transition-opacity duration-200 text-xl px-2 mx-4  hidden sm:block ">
                 Home
+              </p>
+              <p className="font-semibold text-white opacity-70 hover:opacity-100 transition-opacity duration-200 text-xl px-2 mx-4  hidden sm:block">
+                Transfer
+              </p>
+              <p className="font-semibold text-white opacity-70 hover:opacity-100 transition-opacity duration-200 text-xl px-2 mx-4  hidden sm:block">
+                Statement
               </p>
             </div>
             <div className="flex flex-row justify-center items-center">
-              <i className="text-gray-300 fa-solid fa-gear fa-lg text-3xl mx-5 hover:cursor-pointer hover:text-white-400 "></i>{" "}
-              <p className="font-semibold text-gray-300">LOG OUT</p>
+              <i className="text-gray-300 fa-solid fa-gear fa-lg text-3xl px-2 mx-4 hover:cursor-pointer hover:text-white-400 "></i>{" "}
+              <p className="font-semibold text-gray-300 text-l px-2 mx-4">
+                LOG OUT
+              </p>
             </div>
           </div>
         </div>
