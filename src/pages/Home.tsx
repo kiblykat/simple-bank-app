d7680a8298a5f1c935d4e26d0691a7dcecbeeb75
izzat <izzatfadzlon@gmail.com>
2025-02-13 21:26:27 +0800

    Home: update layout and styling for GIC Balance and Recent Activity cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,23 +15,23 @@ const Home = () => {
 
   return (
     <>
-      <div className=" bg-stone-100 h-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          <div className="card bg-base-100 shadow-xl md:col-span-2 lg:col-span-2 mx-12 md:mt-12 border border-gray-300">
+      <div className="bg-stone-100 h-screen">
+        <div className="grid grid-cols-4">
+          <div className="card bg-base-100 shadow-xl col-span-4 md:col-span-2 mx-12 mt-12 border border-gray-300">
             <div className="card-body">
-              <div className="card-title m-auto">GIC Balance</div>
+              <div className="card-title">GIC Balance</div>
               <hr></hr>
-              <div className="m-auto">
-                <h1 className=" text-4xl font-semibold">$60.00</h1>
+              <div className="">
+                <h1 className=" text-6xl font-semibold">$60.00</h1>
               </div>
             </div>
           </div>
-          <div className="card bg-base-100 shadow-xl md:col-span-2 lg:col-span-2 mx-12 mt-4 md:mt-12 border border-gray-300">
+          <div className="card bg-base-100 shadow-xl col-span-4 md:col-span-2 mx-12 mt-4 md:mt-12 border border-gray-300">
             <div className="card-body">
-              <div className="card-title m-auto">Recent Activity</div>
+              <div className="card-title">Recent Activity</div>
               <hr></hr>
-              <div className="m-auto">
-                <h1 className=" text-4xl font-semibold"></h1>
+              <div className="">
+                <h1 className=" text-6xl font-semibold"></h1>
               </div>
             </div>
           </div>
