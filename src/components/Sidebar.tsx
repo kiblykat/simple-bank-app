e795e90a8f567c1eff8278ebabc9549d1acda3d3
izzat <izzatfadzlon@gmail.com>
2025-02-13 22:51:11 +0800

    Sidebar: Add Sidebar component and integrate it into Home page

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import GlobalContext from "../GlobalContext";
+
+const Sidebar = () => {
+  const globalContext = useContext(GlobalContext);
+  const { isMenuOpen } = globalContext;
+  return (
+    <>
+      <div
+        className={`fixed top-24 left-0 h-full w-64 bg-blue-900 z-50 transform transition-transform duration-300 ease-in-out ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } sm:hidden`}
+      >
+        <div className="flex flex-col p-4 space-y-6">
+          <div className="flex flex-col space-y-4">
+            {["Home", "Transfer", "Statement"].map((tab) => (
+              <button
+                key={tab}
+                className={`text-left font-semibold text-white px-4 py-3 rounded-lg hover:bg-white/20 transition-colors duration-200 `}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Sidebar;
