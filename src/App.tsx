594c4850986ec61fef1f925c51f92c45e37be186
izzat <izzatfadzlon@gmail.com>
2025-02-13 11:15:11 +0800

    Landing: Create Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Statement from "./pages/Statement";
 import Transfer from "./pages/Transfer";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <HashRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/statement" element={<Statement />} />
         <Route path="/transfer" element={<Transfer />} />
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <>
+      <div className=" bg-stone-100 h-full">Landing</div>
+    </>
+  );
+};
+
+export default Landing;
