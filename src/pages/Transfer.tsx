786dd8079c6c0d6eb2c73c7b4c66f812c5eddc2d
izzat <izzatfadzlon@gmail.com>
2025-02-14 10:19:04 +0800

    Transfer: create Transfer page for deposit and withdrawal options

--- src/pages/Transfer.tsx
+++ src/pages/Transfer.tsx
@@ -15,7 +15,47 @@ const Transfer = () => {
 
   return (
     <>
-      <div className="bg-stone-100 h-screen"></div>
+      <div className="bg-stone-100 h-screen">
+        <div className="flex flex-row justify-center w-screen h-screen">
+          <div className="bg-white p-4 rounded-lg shadow-md col-start-2">
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="text-xl font-semibold ">Transfer Money</h1>
+            </div>
+            <div className="mt-4 px-10 rounded-full">
+              <p className="font-bold block text-sm text-gray-700 mb-3">
+                Funds In
+              </p>
+              <div
+                onClick={() => navigate("/transfer/deposit")}
+                className="flex flex-row justify-left border border-solid shadow-md rounded-xl p-5 m-5"
+              >
+                <i className="text-3xl text-blue-800 fa-solid fa-arrow-right-arrow-left p-7"></i>
+                <div>
+                  <p className="font-bold mt-3">Deposit money</p>
+                  <div className="text-xs">
+                    Deposit funds into your bank account
+                  </div>
+                </div>
+              </div>
+              <p className="font-bold block text-sm text-gray-700 mb-3">
+                Funds Out
+              </p>
+              <div
+                onClick={() => navigate("/transfer/withdraw")}
+                className="flex flex-row justify-left border border-solid shadow-md rounded-xl p-5 m-5"
+              >
+                <i className="text-3xl text-blue-800 fa-solid fa-arrow-right-arrow-left p-7"></i>
+                <div>
+                  <p className="font-bold mt-3">Withdraw money</p>
+                  <div className="text-xs">
+                    Withdraw funds out of your bank account
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
