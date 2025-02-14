3c88c18e17a7c08f7e37c1e9373f899989e2b47e
izzat <izzatfadzlon@gmail.com>
2025-02-14 22:46:54 +0800

    utils: Add utility function to format dates

---
+++ src/utils/utils.tsx
@@ -0,0 +1,10 @@
+export const formatDate = (date: Date) => {
+  return date.toLocaleString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
