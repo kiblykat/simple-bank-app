f81d517cb5b3517deaff92c2e9a44d8b0e32ff8f
izzat <izzatfadzlon@gmail.com>
2025-02-14 22:33:35 +0800

    Statement: implement Statement page

--- src/pages/Statement.tsx
+++ src/pages/Statement.tsx
@@ -4,8 +4,19 @@ import { useNavigate } from "react-router-dom";
 
 const Statement = () => {
   const navigate = useNavigate();
-  const globalContext = useContext(GlobalContext);
-  const { isLoggedIn, setActiveTab } = globalContext;
+  const { isLoggedIn, setActiveTab, transactions } = useContext(GlobalContext);
+
+  const formatDate = (date: Date) => {
+    return date.toLocaleString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+  };
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -16,7 +27,35 @@ const Statement = () => {
 
   return (
     <>
-      <div>"Statement"</div>
+      <div className="flex flex-row justify-center">
+        <div className="overflow-y-hidden w-full m-10 border-2 rounded-xl shadow-lg">
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="text-sm">Date</th>
+                <th className="text-sm">Amount</th>
+                <th className="text-sm">Balance</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction, index) => (
+                <tr key={index}>
+                  <td>{formatDate(transaction.date)}</td>
+                  <td
+                    className={
+                      transaction.amount > 0 ? "text-success" : "text-error"
+                    }
+                  >
+                    {transaction.amount > 0 ? "+" : "-"}$
+                    {Math.abs(transaction.amount).toFixed(2)}
+                  </td>
+                  <td>${transaction.balance.toFixed(2)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
