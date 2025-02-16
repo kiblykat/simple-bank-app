477137faccc1984552b2d7ae4c0f8d816ff6d496
izzat <izzatfadzlon@gmail.com>
2025-02-16 18:02:43 +0800

    cypress: Add e2e test for depositing using Transfer Funds button

--- cypress/e2e/transfer/TransferMoney.cy.ts
+++ cypress/e2e/transfer/TransferMoney.cy.ts
@@ -1,7 +1,16 @@
 /// <reference types="cypress" />
 
-describe("Transfer money", () => {
-  it("Depositing money", () => {
+describe("Transfer money ", () => {
+  it("Depositing money using Transfer Funds button", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("LOG IN").click();
+    cy.contains("Transfer Funds").click();
+    cy.contains("Deposit money").click();
+    cy.get("input").type("100");
+    cy.get("[data-testid=deposit-button]").click();
+    cy.contains("$100.00 has been deposited to your account").should("exist");
+  });
+  it("Depositing money using Transfer navbar", () => {
     cy.visit("http://localhost:5173/");
     cy.contains("LOG IN").click();
     cy.contains("Transfer").click();
