724696e7f10c08a2aaf6748fe0a2bceef60d5d40
izzat <izzatfadzlon@gmail.com>
2025-02-16 17:55:30 +0800

    cypress: Add end-to-end tests for deposit, withdrawal, and print statement functionality

---
+++ cypress/e2e/transfer/TransferMoney.cy.ts
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe("Transfer money", () => {
+  it("Depositing money", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("LOG IN").click();
+    cy.contains("Transfer").click();
+    cy.contains("Deposit money").click();
+    cy.get("input").type("100");
+    cy.get("[data-testid=deposit-button]").click();
+    cy.contains("$100.00 has been deposited to your account").should("exist");
+  });
+
+  it("Withdrawing money with insufficient funds", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("LOG IN").click();
+    cy.contains("Transfer").click();
+    cy.contains("Withdraw money").click();
+    cy.get("input").type("100");
+    cy.get("[data-testid=withdraw-button]").click();
+    cy.contains("Insufficient funds").should("exist");
+  });
+  it("Withdrawing money with sufficient funds", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("LOG IN").click();
+    cy.contains("Transfer").click();
+    cy.contains("Deposit money").click();
+    cy.get("input").type("100");
+    cy.get("[data-testid=deposit-button]").click();
+    cy.contains("Withdraw money").click();
+    cy.get("input").type("100");
+    cy.get("[data-testid=withdraw-button]").click();
+    cy.contains("$100.00 has been withdrawn from your account").should("exist");
+  });
+});
