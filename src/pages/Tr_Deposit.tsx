1ecd6472e8dee9631b1b8fe7ebb311a4ea110983
Izzat Fadzlon <muhammad.izzat.2.fadzlon@continental-corporation.com>
2025-02-22 19:00:27 +0800

    UI FIX: update background components to use full screen height

--- src/pages/Tr_Deposit.tsx
+++ src/pages/Tr_Deposit.tsx
@@ -42,7 +42,7 @@ const Tr_Deposit = () => {
 
   const navigate = useNavigate();
   return (
-    <div className="bg-stone-100 h-full">
+    <div className="bg-stone-100 h-screen">
       <div className="flex flex-row justify-center">
         <div className="flex flex-col bg-white shadow-lg m-8 rounded-lg border-l-8 border-solid border-blue-300">
           <div className="flex flex-row justify-between">
